eaab8e48e80263a00fce57bbb15cea12abbeb465
DAIGA <dayaxiqi@gmail.com>
2022-04-18 13:44:29 +0900

    会員登録APIの実装

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','App\Http\Controllers\TopPageController@show');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
